474fe36b46ad316866c9d0516731193dbfa2eafb
diegue-exe <dieeegue2104@gmail.com>
2023-05-17 08:09:00 +0200

    refactor: Create new types from theme constants

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,12 @@
-export const background = {
+const background = {
   primary: '#fff',
   secondary: '#f5f5f5',
   accent: '#db4444',
   black: '#000'
 };
+export type Background = keyof typeof background;
 
-export const text = {
+const text = {
   colors: {
     primary: '#000',
     secondary: '#7d8184',
@@ -35,8 +36,10 @@ export const text = {
     }
   }
 };
+export type Color = keyof typeof text.colors;
 
-export const container = {
+const container = {
   maxWidth: '1170px',
   margin: 'auto'
 };
+export type Container = keyof typeof container;
