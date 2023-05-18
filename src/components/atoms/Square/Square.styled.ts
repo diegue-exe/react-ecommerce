49768f2aee38e73b8dc23ea49e3d8f7a0bcae7ab
diegue-exe <dieeegue2104@gmail.com>
2023-05-18 08:57:21 +0200

    feat: Create Square atom for section categories

---
+++ src/components/atoms/Square/Square.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Background } from '../../../theme/theme';
+import { background } from '../../../theme/theme';
+
+type Props = {
+  backgroundColor: Background;
+};
+
+export const StyledSquare = styled.div<Props>`
+  width: 20px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: ${({ backgroundColor }) => background[backgroundColor]};
+`;
