a8085d3c61d1df718cc905f57314a3d8cb3df3d0
diegue-exe <dieeegue2104@gmail.com>
2023-05-25 10:23:54 +0200

    feat: Create ItemDisplay molecule

---
+++ src/components/molecules/ItemDisplay/ItemDisplay.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ItemContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const PricesContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+`;
