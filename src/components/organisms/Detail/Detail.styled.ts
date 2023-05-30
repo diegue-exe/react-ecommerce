3255bb0336d6b6469b5c5584de420f0fd1dba220
diegue-exe <dieeegue2104@gmail.com>
2023-05-30 21:30:04 +0200

    feat: Show some details about the product

--- src/components/organisms/Detail/Detail.styled.ts
+++ src/components/organisms/Detail/Detail.styled.ts
@@ -1,6 +1,20 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
   max-width: ${({ theme }) => theme.container.maxWidth};
   margin: auto;
+  gap: 70px;
+`;
+
+export const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const Underline = styled.aside`
+  width: 100%;
+  height: 1px;
+  background-color: black;
 `;
--- src/components/organisms/Detail/Detail.tsx
+++ src/components/organisms/Detail/Detail.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
 
 import { Product } from '../../../models/Product';
-import { Container } from './Detail.styled';
+import { Container, DetailsContainer, Underline } from './Detail.styled';
 import { ImageGallery } from './components/ImageGallery';
+import { Text } from '../../atoms/Text';
+import { Rating } from '../../atoms/Rating';
 
 type Props = {
   product: Product;
@@ -12,6 +14,16 @@ export const Detail: FC<Props> = ({ product }) => {
   return (
     <Container>
       <ImageGallery product={product} />
+      <DetailsContainer>
+        <Text title={product.description} textSize="big" bold />
+        <Rating score={product.score} amount={product.amountOfRatings} />
+        <Text title={`$${product.price.toFixed(2)}`} textSize="big" />
+        <Text
+          title="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus esse aperiam voluptate exercitationem illum mollitia officiis facere sit voluptas harum"
+          textSize="small"
+        />
+        <Underline></Underline>
+      </DetailsContainer>
     </Container>
   );
 };
